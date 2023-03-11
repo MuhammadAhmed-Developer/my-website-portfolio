a3d98362d0c498f1b8c09f2203634db1abf36d4c
MuhammadAhmed-Developer <ahmadginideveloper@gmail.com>
2023-03-11 22:38:53 +0500

    Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,30 +1,100 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import myImage from '../../assects/images/my image.jpg'
 
 export default function Home() {
+     const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(250);
+    // eslint-disable-next-line
+    const [index, setIndex] = useState(1);
+    const toRotate = ["Web Developer", "Web Designer", "React Web Application Developer", "Google Firebase"];
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+
+        return () => { clearInterval(ticker) };
+        // eslint-disable-next-line
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(200);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
+        } else {
+            setIndex(prevIndex => prevIndex + 1);
+        }
+    }
     return (
         <>
             {/* Top section */}
 
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-lg-4 col-md-6 col-sm-12">
-                        <div className="card bg-secondary text-center p-lg-5 p-md-3 p-sm-2">
+                    <div className="col-lg-4 top-0 col-md-6 col-sm-12">
+                        <div className="card  text-center p-lg-5 p-md-3 p-sm-2">
                             <div className='text-center'>
-                            <img src={myImage} className="card-img-top imageMy border border-5 border-light w-75" alt="myImage"/>
+                            <img src={myImage} className=" card-img-top imageMy border border-5 border-light" alt="myImage"/>
 
                             </div>
                                 <div className="card-body">
                                     <h3 className="card-title text-white fw-bolder">Muhammad Ahmed</h3>
-                                    <h6 className="card-title text-white fw-bolder">Web Application Developer</h6>
-                                    
-                                    {/* <p class="header-subtitle"> <span class="txt-rotate" dataperiod="1000" data-rotate="[ &quot;Web Developer&quot;, &quot;Web Designer&quot;, &quot;Google Firebase Professional&quot; ]"><span class="wrap">Web Develo</span><span class="text-dark">|</span></span></p> */}
-                                    
-                                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                                    <h6 className="card-title text-white fw-bolder mb-4">Web Application Developer</h6>                                     
+                                    <a href="#home" className="mb-2 btn border border-secondary text-secondary btns">Home</a>
+                                    <a href="#about" className="mb-2 btn border border-secondary text-secondary btns">About</a>
+                                    <a href="#certifcate" className="mb-2 certifcate btn border border-secondary text-secondary btns">Certification</a>
+                                    <a href="#portfolio" className="mb-2 btn port border border-secondary text-secondary btns">Portfolio</a>
+                                    <a href="#contact" className="mb-2 btn port border border-secondary text-secondary btns">Contact</a>
                                 </div>
                         </div>
                     </div>
+
+                    <div className="col-lg-6 col-md-6 col-sm-12 mt-sm-4 mt-lg-0 mt-md-0  d-flex justify-content-center align-content-center">
+                      <div className='mx-5 mt-sm-4 mt-lg-0 mt-md-0  d-flex justify-content-center align-items-center flex-column'>
+                        <h3 className='mt-sm-5 mt-lg-0'>Hi There!</h3>
+                        <h1 className='fw-bolder text-white text-center'>I'M Muhammad Ahmed</h1>
+
+                        <p className='header-subtitle text-center fw-bolder fs-3'> <span className="txt-rotate" dataperiod="1000" data-rotate='[ "Web Developer", "Web Designer", "React Web Application Developer" ]'><span className="wrap">{text}</span><span className='text-dark'>|</span></span></p>
+
+                        <a href="#contact" className="mb-2 btn  border border-secondary text-secondary btns">See CV</a>
+                      </div>
+
+                    </div>
+                    <div className="col-2  d-flex justify-content-center align-content-center">
+                        <div className='d-flex justify-content-center align-items-center flex-column'>
+                            <ul className='list'>
+                                <li className='mb-2'><a href="mailto:muhammadahmedite@gmail.com" target='_blank'><i class="bi bi-envelope text-secondary fs-4"></i></a></li>
+
+                                <li className='mb-2'><a href="" target='_blank'><i class="bi bi-linkedin text-secondary fs-4"></i></a></li>
+                                <li className='mb-2'><a href="" target='_blank'><i class="bi bi-facebook text-secondary fs-4"></i></a></li>
+                                <li className='mb-2'><a href="" target='_blank'><i class="bi bi-instagram text-secondary fs-4"></i></a></li>
+                                <li className='mb-2'><a href="" target='_blank'><i class="bi bi-github text-secondary fs-4"></i></a></li>
+                                <li className='mb-2'><a href="" target='_blank'><i class="bi bi-whatsapp text-secondary fs-4"></i></a></li>
+                                <li className='mb-2'><a href="" target='_blank'><i class="bi bi-twitter text-secondary fs-4"></i></a></li>
+                                <li className='mb-2'><a href="" target='_blank'><i class="bi bi-youtube text-secondary fs-4"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
+
             </div>
 
 
